1b22f96d9554210cf573a5f556590bf00b9bcf51
mrtaalebi <the.doors.are.locked@gmail.com>
2019-11-01 18:48:09 +0330

    check installation path

--- install.sh
+++ install.sh
@@ -18,8 +18,19 @@ ${package_manager} git openconnect
 
 git clone https://github.com/mrtaalebi/occ
 cd occ
-sudo cp occ /usr/bin/occ
+
+copy_to=/usr/bin/occ
+if  [ -f $copy_to ] || [ -d $copy_to ]; then
+    echo "a file or directory named occ already exists in $copy_to."
+    echo "try checking and removing it before running installation script again."
+    echo "exiting."
+    exit 1
+fi
+
+sudo cp occ $copy_to
 sudo chmod +x /usr/bin/occ
+cd ..
+rm -rf occ
 
 echo
 echo "occ successfully installed."
